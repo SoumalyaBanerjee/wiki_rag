970308dd5d474918795eaee61449632c9d0c8e61
SoumalyaBanerjee <40776477+SoumalyaBanerjee@users.noreply.github.com>
2026-03-09 01:15:59 +0530

    adding a few pip install changes

---
+++ scripts/0.start.py
@@ -0,0 +1,7 @@
+# This file contains shell scripts to start 
+# .\.venv\Scripts\Activate.ps1
+# find docker desktop and run it in windows PC
+# Write-Host "🔹 Starting Docker containers..."
+# docker start qdrant
+# docker start minio
+# pip install -r requirements.txt
